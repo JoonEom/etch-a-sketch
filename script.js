15a540220c85074fc73347c25b6d8ce4e2fb92ed
Minjoon Eom <eomminjoon81@gmail.com>
2023-09-12 18:04:53 -0700

    Finished ui and DOM

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const grid = document.querySelector('.grid-container');
 const gridCell = document.getElementsByClassName('cell');
+const reset = document.querySelector('.reset');
+const resolution = document.querySelector('.resolution');
 let gridWidth = 16;
 
 
@@ -10,6 +12,7 @@ function createGrid(gridWidth)
     {
         let tempCell = document.createElement("div");
         tempCell.classList.add('cell');
+        //css style for grid
         grid.style.gridTemplateColumns = `repeat(${gridWidth}, 1fr)`;
         grid.style.gridTemplateRows = `repeat(${gridWidth}, 1fr)`;
         grid.insertAdjacentElement('beforeend', tempCell);
@@ -17,11 +20,39 @@ function createGrid(gridWidth)
     }
     for(let i = 0; i < gridArea; i++)
     {
-        gridCell[i].addEventListener('mouseenter', function (event) {
-            event.target.style.backgroundColor = "black";
+        gridCell[i].addEventListener('mouseenter', function (e) {
+            e.target.style.backgroundColor = "black";
         })
     }
 }
+
+function resetGrid(){
+    for (let i=0; i < (gridWidth * gridWidth); i++) {
+        gridCell[i].style.backgroundColor = "white";
+    }
+}
+
+reset.addEventListener("click", function () {
+    for (let i=0; i < (gridWidth * gridWidth); i++) {
+        gridCell[i].style.backgroundColor = "white";
+    }
+
+},false);
+
+resolution.addEventListener("click",function(){
+    let userInput = prompt("Enter a number 1-100: ");
+    while(userInput > 100 || userInput < 1 || isNaN(userInput))
+    {
+        userInput = prompt("Not in range. Please enter again: ")
+    }
+    gridWidth = userInput;
+    createGrid(gridWidth);
+    resetGrid();
+    
+});
+
+
+
+
 createGrid(gridWidth);
-  
   
